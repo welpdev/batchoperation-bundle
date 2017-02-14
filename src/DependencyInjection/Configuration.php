42e91cbdd914530188d2b19b3ec932dc884b4b53
gaetan <gaetan@welp.today>
2017-02-14 14:51:32 +0100

    fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Welp\IcalBundle\DependencyInjection;
+namespace Welp\BatchBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
